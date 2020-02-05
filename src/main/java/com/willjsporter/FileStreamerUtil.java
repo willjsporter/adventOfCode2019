d7c186bfbd67d241b25b0d7ef0f38c2e9dd0b087
Will <willjsporter@hotmail.co.uk>
2020-02-05 17:51:45 +0000

    feat(file-reader): add utility method to read lines of a file as integers

--- src/main/java/com/willjsporter/FileStreamerUtil.java
+++ src/main/java/com/willjsporter/FileStreamerUtil.java
@@ -3,17 +3,22 @@ package com.willjsporter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class FileStreamerUtil {
 
-  public static Stream<String> streamFile(String filepath) {
+    static Stream<String> streamFile(String filepath) {
       String projectRootDirectory = System.getProperty("user.dir") + "/";
       try {
           return Files.lines(Paths.get(projectRootDirectory + filepath));
       } catch (IOException e) {
           return Stream.of(e.getMessage());
       }
-  }
+}
 
+    public static IntStream fileLinesAsIntegers(String filepath) {
+        return streamFile(filepath)
+            .mapToInt(Integer::valueOf);
+    }
 }
--- src/test/java/com/willjsporter/FileStreamerUtilTest.java
+++ src/test/java/com/willjsporter/FileStreamerUtilTest.java
@@ -3,8 +3,11 @@ package com.willjsporter;
 import org.junit.Test;
 
 import java.util.List;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertTrue;
 
 public class FileStreamerUtilTest {
@@ -15,6 +18,15 @@ public class FileStreamerUtilTest {
     List<String> expectedFileContents = List.of("hello", "world", "testing");
 
     boolean doFileContentsMatchExpectedList = fileStream.allMatch(expectedFileContents::contains);
+    assertTrue(doFileContentsMatchExpectedList);
+  }
+
+  @Test
+  public void shouldBeAbleToGetIntegersFromFileLines() {
+    IntStream fileAsIntStream = FileStreamerUtil.fileLinesAsIntegers("src/test/resources/integerTestInput.txt");
+    List<Integer> expectedFileContentsAsIntegers = List.of(2, 11, 111, 1111);
+
+    boolean doFileContentsMatchExpectedList = fileAsIntStream.allMatch(expectedFileContentsAsIntegers::contains);
     assertTrue(doFileContentsMatchExpectedList);
   }
 }
