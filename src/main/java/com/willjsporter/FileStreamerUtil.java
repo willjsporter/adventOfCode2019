8f15a7a5e0670fcd4f9938c413d4518dfb365d09
Will Porter <willjsporter@hotmail.co.uk>
2020-02-18 12:06:57 +0000

    feat(filereader): can put a file containing a list of integers into an intstream

--- src/main/java/com/willjsporter/FileStreamerUtil.java
+++ src/main/java/com/willjsporter/FileStreamerUtil.java
@@ -1,6 +1,7 @@
 package com.willjsporter;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.stream.IntStream;
@@ -8,17 +9,30 @@ import java.util.stream.Stream;
 
 public class FileStreamerUtil {
 
-    static Stream<String> streamFile(String filepath) {
-      String projectRootDirectory = System.getProperty("user.dir") + "/";
-      try {
-          return Files.lines(Paths.get(projectRootDirectory + filepath));
-      } catch (IOException e) {
-          return Stream.of(e.getMessage());
-      }
-}
+    static IntStream streamFileInputAsIntegers(String filepath) {
+        String projectRootDirectory = System.getProperty("user.dir") + "/";
+        try {
+            String fileContents = Files
+                .readString(Paths.get(projectRootDirectory + filepath), StandardCharsets.UTF_8)
+                .trim();
+            return Stream.of(fileContents.split(","))
+                .mapToInt(Integer::valueOf);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    static Stream<String> streamFileLines(String filepath) {
+        String projectRootDirectory = System.getProperty("user.dir") + "/";
+        try {
+            return Files.lines(Paths.get(projectRootDirectory + filepath));
+        } catch (IOException e) {
+            return Stream.of(e.getMessage());
+        }
+    }
 
     public static IntStream fileLinesAsIntegers(String filepath) {
-        return streamFile(filepath)
+        return streamFileLines(filepath)
             .mapToInt(Integer::valueOf);
     }
 }
--- src/test/java/com/willjsporter/FileStreamerUtilTest.java
+++ src/test/java/com/willjsporter/FileStreamerUtilTest.java
@@ -6,15 +6,22 @@ import java.util.List;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertTrue;
 
 public class FileStreamerUtilTest {
 
   @Test
+  public void shouldPutListOfIntegersFromFileIntoStream() {
+    IntStream fileStream = FileStreamerUtil.streamFileInputAsIntegers("src/test/resources/integerTestInput_List.txt");
+    List<Integer> expectedFileContents = List.of(1, 2, 5, 6, 2, 4, 200);
+
+    boolean doFileContentsMatchExpectedList = fileStream.allMatch(expectedFileContents::contains);
+    assertTrue(doFileContentsMatchExpectedList);
+  }
+
+  @Test
   public void shouldPutFileLinesIntoStream() {
-    Stream<String> fileStream = FileStreamerUtil.streamFile("src/test/resources/stringTestInput.txt");
+    Stream<String> fileStream = FileStreamerUtil.streamFileLines("src/test/resources/stringTestInput_Lines.txt");
     List<String> expectedFileContents = List.of("hello", "world", "testing");
 
     boolean doFileContentsMatchExpectedList = fileStream.allMatch(expectedFileContents::contains);
@@ -23,7 +30,7 @@ public class FileStreamerUtilTest {
 
   @Test
   public void shouldBeAbleToGetIntegersFromFileLines() {
-    IntStream fileAsIntStream = FileStreamerUtil.fileLinesAsIntegers("src/test/resources/integerTestInput.txt");
+    IntStream fileAsIntStream = FileStreamerUtil.fileLinesAsIntegers("src/test/resources/integerTestInput_Lines.txt");
     List<Integer> expectedFileContentsAsIntegers = List.of(2, 11, 111, 1111);
 
     boolean doFileContentsMatchExpectedList = fileAsIntStream.allMatch(expectedFileContentsAsIntegers::contains);
