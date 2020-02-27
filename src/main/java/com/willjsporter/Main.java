7bbb33263a0342c3cccc1980a96debb6ed1ded18
Will <willjsporter@hotmail.co.uk>
2020-02-27 19:48:31 +0000

    feat(day2solution): use for loop to get answer to day2b problem

--- src/main/java/com/willjsporter/Main.java
+++ src/main/java/com/willjsporter/Main.java
@@ -1,10 +1,23 @@
 package com.willjsporter;
 
-import java.util.stream.IntStream;
+import com.willjsporter.util.FileStreamerUtil;
+
+import java.util.List;
 
 public class Main {
   public static void main(String[] args) {
-    IntStream intStream = FileStreamerUtil.fileLinesAsIntegers("puzzleInput/day1a.txt");
-    System.out.println(new FuelCounterUpper().calculateFuelRequiredForFleet(intStream));
+    List<Integer> day2Input = FileStreamerUtil.streamFileInputAsIntegers("puzzleInput/day2a.txt");
+    int _100TimesNounTimesVerb = 0;
+    for(int i = 0; i < day2Input.size(); i ++) {
+      for(int j = 0; j < day2Input.size(); j++) {
+        day2Input.set(1, i);
+        day2Input.set(2, j);
+        if (new IntcodeProgram(day2Input).run().get(0) == 19690720) {
+          _100TimesNounTimesVerb = 100 * day2Input.get(1) + day2Input.get(2);
+          break;
+        }
+      }
+    }
+    System.out.println(_100TimesNounTimesVerb);
   }
 }
