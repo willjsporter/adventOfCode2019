6acffda8f37d04414d07a939f81ebff2c4ce5b85
Will <willjsporter@hotmail.co.uk>
2020-02-27 00:21:41 +0000

    test(intcode-program): add extra test to try to diagnose issues

--- src/test/java/com/willjsporter/IntcodeProgramTest.java
+++ src/test/java/com/willjsporter/IntcodeProgramTest.java
@@ -27,9 +27,15 @@ public class IntcodeProgramTest {
     }
 
     @Test
-    public void givenOpcode99_thenProgramShouldHaltImmediately() {
+    public void givenOpcode99AtStartOfInput_thenProgramShouldHaltImmediately() {
         IntcodeProgram intcodeProgram = new IntcodeProgram(List.of(99, 1, 2, 3, 4, 5, 6, 299));
         assertThat(intcodeProgram.run(), is(List.of(99, 1, 2, 3, 4, 5, 6, 299)));
+    }
+
+    @Test
+    public void givenOpcode99InMiddleOfInput_thenProgramShouldHaltWhen99Reached() {
+        IntcodeProgram intcodeProgram = new IntcodeProgram(List.of(1, 1, 1, 1, 99, 1, 6, 0, 3, 2, 3, 4, 5));
+        assertThat(intcodeProgram.run(), is(List.of(1, 2, 1, 1, 99, 1, 6, 0, 3, 2, 3, 4, 5)));
     }
 
     @Test
@@ -42,6 +48,12 @@ public class IntcodeProgramTest {
     public void givenMultipleOpcodes_thenProgramShouldIterateThroughThem2() {
         IntcodeProgram intcodeProgram = new IntcodeProgram(List.of(1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50));
         assertThat(intcodeProgram.run(), is(List.of(3500, 9, 10, 70, 2, 3, 11, 0, 99, 30, 40, 50)));
+    }
+
+    @Test
+    public void givenMultipleOpcodes_thenProgramShouldIterateThroughThem3() {
+        IntcodeProgram intcodeProgram = new IntcodeProgram(List.of(1, 1, 1, 4, 99, 5, 6, 0, 99));
+        assertThat(intcodeProgram.run(), is(List.of(30, 1, 1, 4, 2, 5, 6, 0, 99)));
     }
 
     @Test
