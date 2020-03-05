224d78edcfc97319e15d6aa5e7f6eabb22307579
Will Porter <willjsporter@hotmail.co.uk>
2020-03-05 23:55:24 +0000

    test(wire): add integration test to read wirepath from instructions file

---
+++ src/test/java/com/willjsporter/WireIntegrationTest.java
@@ -0,0 +1,52 @@
+package com.willjsporter;
+
+import com.willjsporter.util.FileStreamerUtil;
+import org.junit.Test;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+public class WireIntegrationTest {
+
+    @Test
+    public void givenInstructionsFromFile_shouldReadThem_thenPopulateThemToWireLocation() {
+        List<Stream<String>> wirePaths = FileStreamerUtil.streamFileInputAsDirections("src/test/resources/directionsTestInput.txt");
+        Wire firstWire = new Wire(wirePaths.get(0));
+        Wire secondWire = new Wire(wirePaths.get(1));
+
+        assertThat(firstWire.getWirePath(), is(
+            List.of(
+                Coordinate.of(0, 1),
+                Coordinate.of(-1, 1),
+                Coordinate.of(-2, 1),
+                Coordinate.of(-3, 1),
+                Coordinate.of(-3, 0),
+                Coordinate.of(-3, -1),
+                Coordinate.of(-3, -2),
+                Coordinate.of(-3, -3),
+                Coordinate.of(-3, -4),
+                Coordinate.of(-2, -4),
+                Coordinate.of(-2, -3),
+                Coordinate.of(-2, -2)
+            )));
+        assertThat(secondWire.getWirePath(), is(
+            List.of(
+                Coordinate.of(0, -1),
+                Coordinate.of(0, -2),
+                Coordinate.of(0, -3),
+                Coordinate.of(-1, -3),
+                Coordinate.of(-2, -3),
+                Coordinate.of(-3, -3),
+                Coordinate.of(-4, -3),
+                Coordinate.of(-4, -2),
+                Coordinate.of(-3, -2),
+                Coordinate.of(-2, -2),
+                Coordinate.of(-1, -2),
+                Coordinate.of(0, -2),
+                Coordinate.of(1, -2)
+            )));
+    }
+}
