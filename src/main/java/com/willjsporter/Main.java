14f99b2234c6874e8a7c1fb0a2705e3bdba36745
Will <willjsporter@hotmail.co.uk>
2020-03-12 15:09:04 +0000

    feat(day4b): main method to get solution for day 4

--- src/main/java/com/willjsporter/Main.java
+++ src/main/java/com/willjsporter/Main.java
@@ -1,28 +1,12 @@
 package com.willjsporter;
 
-import com.willjsporter.util.FileStreamerUtil;
-
-import java.util.List;
-import java.util.stream.Stream;
+import java.util.stream.IntStream;
 
 class Main {
     public static void main(String[] args) {
-      List<Stream<String>> wireInstructions = FileStreamerUtil.streamFileInputAsDirections("puzzleInput/day3a.txt");
       long start = System.currentTimeMillis();
-      Wire wire1 = new Wire(wireInstructions.get(0));
-      long mid = System.currentTimeMillis();
-      System.out.println("first wire took " + (mid - start) + " ms to load");
-      Wire wire2 = new Wire(wireInstructions.get(1));
+      System.out.println(IntStream.rangeClosed(273025, 767253).filter(PasswordCracker::checkPassword).count());
       long end = System.currentTimeMillis();
-      System.out.println("second wire took " + (end - mid) + "ms to load");
-
-      Coordinate intersection = wire1.getIntersectionPointWithLeastSteps(wire2);
-      Integer intersectionSteps = wire1.getNumberOfStepsToIntersectionWithLeastSteps(wire2);
-
-      System.out.println("x = " + intersection.getX() + ", y = " + intersection.getY());
-      System.out.println("number of steps to reach intersection is: " + intersectionSteps);
-      long intersectioncalctime = System.currentTimeMillis();
-      System.out.println("intersection working time was " + (intersectioncalctime - end) + "millis");
-      System.out.println(intersection.manhattanLength());
+      System.out.println("solution took " + (end - start) + " ms to load");
     }
 }
